918d7f9fabeb04951708a76e266fdcae7594c5df
Callum/PC <callumowen6@googlemail.com>
2021-01-17 22:46:45 +0000

    84 - Imported assets, terrain made, splash screen with music player, and basic circuit

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+	// Awake is called on scene load
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        Invoke("LoadFirstScene", 3.0f);
+    }
+
+    void LoadFirstScene()
+	{
+        SceneManager.LoadScene(1);
+	}
+}
